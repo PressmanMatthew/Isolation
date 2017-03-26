46c4b85ab90cf462338ab3e6d71cd4cfc5b8e5de
Tetyana Kolomiyets <txk6171@rit.edu>
2017-03-26 17:57:22 -0400

    MiniGames
    Timing Game works, Card Match half-way done.

---
+++ Isolation/Assets/Scripts/MatchingGameScript.js
@@ -0,0 +1,83 @@
+﻿
+public var card1 : GameObject;  		// Game Object: Platform Prefab
+public var card2 : GameObject;  		// Game Object: Platform Prefab
+public var card3 : GameObject;  		// Game Object: Platform Prefab
+public var card4 : GameObject;  		// Game Object: Platform Prefab
+public var cardBlank : GameObject;  	// Game Object: Platform Prefab
+
+private var cards : GameObject[];		//array to hold all cards
+
+private var cardWidth: float;
+private var cardHeight: float;
+
+//window size is 740, 560
+//740/5 = 148
+function Start () {
+
+	var render = card1.GetComponent("Renderer"); //save the width and height of cards
+	cardWidth = render.bounds.size.x/2;
+	cardHeight = render.bounds.size.z/2;
+
+	for(var i=0; i<8; i++){ //create teh cards
+		if(i<2)
+			Instantiate(card1, Vector3(i*2, 0, 0), Quaternion.Euler(90f,0,0));
+		else if(i<4)
+			Instantiate(card2, Vector3(i*2, 0, 0), Quaternion.Euler(90f,0,0));
+		else if(i<6)
+			Instantiate(card3, Vector3(i*2-8, 0, -2.5), Quaternion.Euler(90f,0,0));
+		else
+			Instantiate(card4, Vector3(i*2-8, 0, -2.5), Quaternion.Euler(90f,0,0));
+	}
+
+	cards = GameObject.FindGameObjectsWithTag("Card"); //save all cards to the array
+
+	Shuffle(); 
+
+	Hide();
+	
+}
+
+function Update () {
+	if(Input.GetMouseButtonDown(0))
+		OnMouseDown();
+}
+
+function Shuffle(){
+	for(var i=0; i<cards.length/2; i++){
+		var holder1 = cards[i].transform.position;
+
+		var rand = Random.Range(0, cards.length);
+		if(i == rand)
+			rand = Random.Range(0, cards.length);
+
+		var holder2 = cards[rand].transform.position;
+
+		cards[i].transform.position = holder2;
+		cards[rand].transform.position = holder1;
+	}
+}
+
+function Hide(){ //puts a blank card over the front facing ones
+	for(var i=0; i<cards.length; i++){
+		Instantiate(cardBlank, Vector3(cards[i].transform.position.x, 0.01, cards[i].transform.position.z), Quaternion.Euler(90f,0,0));
+	}
+}
+
+public function OnMouseDown(){ //checks the position of the mouse when clicked
+	if(Input.mousePosition.x < 740/4 && Input.mousePosition.y < 560/2)
+		Debug.Log("1st bottom");
+	else if(Input.mousePosition.x < 740/4 && Input.mousePosition.y > 560/2)
+		Debug.Log("1st top");
+	else if(Input.mousePosition.x < 740/2 && Input.mousePosition.y < 560/2)
+		Debug.Log("2nd bottom");
+	else if(Input.mousePosition.x < 740/2 && Input.mousePosition.y > 560/2)
+		Debug.Log("2nd top");
+	else if(Input.mousePosition.x > 740-(740/4) && Input.mousePosition.y < 560/2)
+		Debug.Log("4th bottom");
+	else if(Input.mousePosition.x > 740-(740/4) && Input.mousePosition.y > 560/2)
+		Debug.Log("4th top");
+	else if(Input.mousePosition.x > 740/2 && Input.mousePosition.y < 560/2)
+		Debug.Log("3rd bottom");
+	else if(Input.mousePosition.x > 740/2 && Input.mousePosition.y > 560/2)
+		Debug.Log("3rd top");
+}
---
+++ Isolation/Assets/Scripts/TimingGameScript.js
@@ -0,0 +1,92 @@
+﻿public var successWidth : float;			//set the starting width 
+public var barSpeed : float;				//sets the starting speed
+
+private var maxBar: GameObject ;			//the full bar
+private var successBar: GameObject;			//the success bar
+private var cursor: GameObject;				//the cursor
+
+private var speed: float;					//speed of the cursor
+private var leftMax: float;					//max movement left position
+private var rightMax: float;				//max movement right position
+
+private var successLeft: float;				//max success left position
+private var successRight: float;			//max success right position
+private var stopped : boolean;				//keeps track of whether the cursor is moving or not
+private var scorred : boolean;				//tracks if the cursor stopped in teh green
+
+function Start () {
+	stopped = false;
+	scorred = false;
+
+	//----------------	create the bars
+	maxBar = GameObject.CreatePrimitive(PrimitiveType.Plane);
+	maxBar.transform.position = Vector3(0, 0, -4.02);
+	maxBar.transform.localScale = Vector3(1,1,0.064); 
+	maxBar.GetComponent.<Renderer>().material.color = Color.red;
+
+	successBar = GameObject.CreatePrimitive(PrimitiveType.Plane);
+	successBar.transform.position = Vector3(0, 0.001, -4.02);
+	successBar.transform.localScale = Vector3((successWidth/10),1,0.064); 
+	successBar.GetComponent.<Renderer>().material.color = Color.green;
+
+	cursor = GameObject.CreatePrimitive(PrimitiveType.Plane);
+	cursor.transform.position = Vector3(0, 0.002, -4.02);
+	cursor.transform.localScale = Vector3(0.01,1,0.064); 
+	cursor.GetComponent.<Renderer>().material.color = Color.black;
+
+	//sets the speed of the cursor
+	speed = barSpeed/100;
+
+	var render = maxBar.GetComponent("Renderer");
+	var render2 = successBar.GetComponent("Renderer");
+
+	// sets bounds for the cursor to move between
+	leftMax = maxBar.transform.position.x - render.bounds.size.x/2;
+	rightMax = maxBar.transform.position.x + render.bounds.size.x/2;
+
+	//sets success bounds
+	successLeft = successBar.transform.position.x - render2.bounds.size.x/2;
+	successRight = successBar.transform.position.x + render2.bounds.size.x/2;
+
+}
+
+function Update () {
+
+	//----------------CURSOR MOVEMENT
+	if(cursor.transform.position.x <= leftMax){
+		speed *= -1;
+	}
+	else if(cursor.transform.position.x >= rightMax){
+		speed *= -1;
+	}
+
+	cursor.transform.position.x += speed;
+
+	//----------------INPUT HANDLING
+	if (Input.GetKeyDown(KeyCode.Space) && !stopped){
+		stopped = true;
+		speed = 0;
+
+		if(cursor.transform.position.x > successLeft && cursor.transform.position.x < successRight){
+			barSpeed *= 1.25; //increases the speed of the cursor
+			scorred = true;
+			Debug.Log("WIN!");
+		}
+		else
+			scorred = false;
+	}
+	else if(Input.GetKeyDown(KeyCode.Space) && stopped){
+		if(scorred){	//the green bar ONLY shrinks if the player won
+			var render2 = successBar.GetComponent("Renderer");
+
+			successWidth *= 0.75F;
+			successBar.transform.localScale = Vector3((successWidth/10),1,0.064);
+			successLeft = successBar.transform.position.x - render2.bounds.size.x/2;
+			successRight = successBar.transform.position.x + render2.bounds.size.x/2;
+		}
+		// resumes movement of the cursor
+		stopped = false;
+		speed = barSpeed/100;
+	}
+
+}
