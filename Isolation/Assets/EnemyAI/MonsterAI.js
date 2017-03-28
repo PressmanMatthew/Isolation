5df505591f1bf23e74f66f7dcf25572b0df02f77
Ryan Muskopf (RIT Student) <rjm9187@ad.rit.edu>
2017-03-28 18:27:41 -0400

    added a system to find all of the room centers then give the monster the ability to find the closest one

---
+++ Isolation/Assets/EnemyAI/MonsterAI.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+
+var testObject = 2;
+var centersList;
+var currentTarget;
+
+function Start () {
+
+	var centers : GameObject[];
+
+	centers = GameObject.FindGameObjectsWithTag("roomCenter");
+
+	centersList = centers;
+
+	var roomNum = 1;
+	for(var i : GameObject in centers){
+		i.name = "room"+roomNum+"center";
+
+		roomNum++;
+	}
+
+}
+
+function Update () {
+	if(!currentTarget){
+		print("Finding new center");
+		currentTarget = findNearestCenter();
+	}
+}
+
+function findNearestCenter () {
+	var closest : GameObject;
+	var dist = Mathf.Infinity;
+	var position = transform.position;
+
+	for(var i : GameObject in centersList){
+		var diff = (i.transform.position - position);
+		var curDistance = diff.sqrMagnitude;
+
+		if(curDistance < dist) {
+			closest = i;
+			dist = curDistance;
+		}
+	}
+
+	return closest;
+}
