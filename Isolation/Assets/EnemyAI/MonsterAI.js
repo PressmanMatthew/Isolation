7edba1e44b4d3353ad6655d000d41295d38d9591
Ryan Muskopf (RIT Student) <rjm9187@ad.rit.edu>
2017-03-28 22:21:52 -0400

    alien wanders around base

--- Isolation/Assets/EnemyAI/MonsterAI.js
+++ Isolation/Assets/EnemyAI/MonsterAI.js
@@ -1,8 +1,10 @@
 ﻿#pragma strict
 
-var testObject = 2;
+var speed = 2;
 var centersList;
-var currentTarget;
+var previousPreviousTarget : GameObject;
+var previousTarget : GameObject;
+var currentTarget : GameObject;
 
 function Start () {
 
@@ -24,8 +26,12 @@ function Start () {
 function Update () {
 	if(!currentTarget){
 		print("Finding new center");
+		previousPreviousTarget = previousTarget;
+		previousTarget = currentTarget;
 		currentTarget = findNearestCenter();
 	}
+
+	seek(currentTarget);
 }
 
 function findNearestCenter () {
@@ -44,4 +50,33 @@ function findNearestCenter () {
 	}
 
 	return closest;
+}
+
+function findNextCenter() {
+
+	var nextTarget : GameObject;
+
+	//Debug.Log(currentTarget.GetComponents(Component)[1]);
+	//Debug.Log(currentTarget.GetComponents(roomCenterScript));
+	//var currentScript = currentTarget.GetComponents(roomCenterScript) as roomCenterScript;
+	var currentScript = currentTarget.GetComponent(typeof(roomCenterScript))as roomCenterScript;
+
+	nextTarget = currentScript.next(previousPreviousTarget);
+
+	return nextTarget;
+}
+
+function seek (target : GameObject) {
+
+	var seekVector = (target.transform.position - transform.position);
+	var seekVectorNorm = seekVector.normalized;
+	transform.position += (seekVectorNorm *speed*.01);
+
+	if(seekVector.sqrMagnitude < 3){
+		previousPreviousTarget = previousTarget;
+		previousTarget = currentTarget;
+		currentTarget = findNextCenter();
+	}
+
+	Debug.DrawLine(transform.position, target.transform.position, Color.red);
 }
--- Isolation/Assets/EnemyAI/roomCenterScript.js
+++ Isolation/Assets/EnemyAI/roomCenterScript.js
@@ -1,9 +1,51 @@
 ﻿#pragma strict
 
+var neighborN: GameObject;
+var neighborE: GameObject;
+var neighborS: GameObject;
+var neighborW: GameObject;
+var neighbors = new Array();
+
 function Start () {
-	
+
+	if(neighborN){
+		neighbors.Push(neighborN);
+	}
+	if(neighborE){
+		neighbors.Push(neighborE);
+	}
+	if(neighborS){
+		neighbors.Push(neighborS);
+	}
+	if(neighborW){
+		neighbors.Push(neighborW);
+	}
+
 }
 
 function Update () {
 	
+}
+
+function next(previousTarget: GameObject){
+	var nextTarget : GameObject;
+
+	var nextRand = Random.value*neighbors.length;
+	var nextNum = Mathf.Floor(nextRand);
+
+	Debug.Log(nextNum);
+
+	nextTarget = neighbors[nextNum];
+
+	if(nextTarget == previousTarget){
+		nextNum++;
+
+		if(nextNum >=neighbors.length){
+			nextNum = 0;
+		}
+
+		nextTarget = neighbors[nextNum];
+	}
+
+	return nextTarget;
 }
